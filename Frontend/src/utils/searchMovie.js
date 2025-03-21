7986382610461d70546f22765b4a249d8e93bbb5
Kaustav Mishra <160534780+km-git007@users.noreply.github.com>
2025-03-21 13:45:56 +0530

    Fix minor bug in movie api

--- Frontend/src/utils/searchMovie.js
+++ Frontend/src/utils/searchMovie.js
@@ -8,15 +8,11 @@ const searchMovieTMDB = async (movie) => {
       "&include_adult=false&language=en-US&page=1&sort_by=popularity.desc"
    );
 
-   // console.log(data.data.results);
    const fetchedMovie = data?.data?.results[0];
 
    if (!fetchedMovie) {
       throw new Error(`No movie found for query: ${movie}`);
    }
-
-   // console.log(fetchedMovie);
-
    return fetchedMovie;
 };
 
